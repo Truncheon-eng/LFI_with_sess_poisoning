6ab3b77d3fe7a6adf948964c8884336b0ece8cd8
Truncheon-eng <146008203+Truncheon-eng@users.noreply.github.com>
2025-01-22 18:26:35 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -32,7 +32,7 @@ $_SESSION['language'] = $path_to_lang_file;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Language Selection</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="/style.css" type="text/css">
 </head>
 <body>
     <div class="container">
